509fb1a4f999603eafcd4aa85ba5a3659d0f2ead
Alan Corona López <88749310+AlanCoronaaa@users.noreply.github.com>
2025-09-18 11:14:32 -0600

    version 2

--- Code.py
+++ Code.py
@@ -18,4 +18,22 @@ df_scaled = scaler.fit_transform(df)
 silhouette_scores = []
 for k in range(2, 10):
     kmeans = KMeans(n_clusters=k)
-    cluster_labels = kmeans
+    cluster_labels = kmeans.fit_predict(df_scaled)
+    silhouette_avg = silhouette_score(df_scaled, cluster_labels)
+    silhouette_scores.append(silhouette_avg)
+
+plt.plot(range(2, 10), silhouette_scores)
+plt.xlabel('Valor de k')
+plt.ylabel('Puntuación de Silhouette')
+plt.show()
+
+# Seleccionar valor de k
+k = 5
+
+# Calcular centros del algoritmo k-means
+kmeans = KMeans(n_clusters=k)
+kmeans.fit(df_scaled)
+centros = kmeans.cluster_centers_
+
+print("Centros:")
+print(centros)
